eb502e71dd8125287339886e7e70d229f7d6505b
Fernando <Fernando@DESKTOP-Fernando>
2020-05-17 20:00:22 -0300

    Comando de mensagens privadas e correcao de bugs

--- src/Servidor/GerenciadorDeCliente.java
+++ src/Servidor/GerenciadorDeCliente.java
@@ -5,35 +5,66 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.glass.events.KeyEvent;
 
 public class GerenciadorDeCliente extends Thread {
 	
 	private Socket cliente;
 	private String nomeCliente;
+	private BufferedReader leitor;
+	private PrintWriter escritor;
+	private static final Map<String, GerenciadorDeCliente> usuarios = new HashMap<String, GerenciadorDeCliente>();
 
 	public GerenciadorDeCliente(Socket cliente) {
 		this.cliente = cliente;
 		start();
-	}	
+	}
 	
 	@Override
 	public void run() {
+		
+		/*INICIALIZACAO DO MODELO DE ESCRITA E LEITURA DE MENSAGENS ENTRE CLIENTES*/
 		try {
-			
-			BufferedReader leitor = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-			PrintWriter escritor = new PrintWriter(cliente.getOutputStream(), true); /*PRESTAR ATENCAO NO AUTOFLUSH*/
+			leitor = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+			escritor = new PrintWriter(cliente.getOutputStream(), true); /*PRESTAR ATENCAO NO AUTOFLUSH*/
 			escritor.println("Qual e o seu nome?");
 			String msg = leitor.readLine();
 			this.nomeCliente = msg;
 			escritor.println("Bem vindo (" + this.nomeCliente + ")" + " escreva alguma coisa!");
+			usuarios.put(this.nomeCliente, this);
 			
 			while(true) {
+				
 				msg = leitor.readLine();
+				
+				/* COMANDO SAIR */
 				if (msg.equalsIgnoreCase("/sair")) {
 					this.cliente.close();
-				}else {
+				}
+				/* COMANDO DE MENSAGEM PRIVADA */
+				else if(msg.startsWith("/") && msg.contains(" ")){
+					
+					String[] mensagemPrivada = msg.split(" ", 2);
+					String nomeDestinatario = mensagemPrivada[0].substring(mensagemPrivada[0].lastIndexOf("/") + 1);
+					escritor.println("(" + this.nomeCliente + ") disse para (" + nomeDestinatario + "): " + mensagemPrivada[1]);
+					GerenciadorDeCliente destinatario = usuarios.get(nomeDestinatario);
+					
+					if (destinatario == null) {
+						escritor.println("Usuario inexistente");
+					}else {
+						destinatario.getEscritor().println("(" + this.nomeCliente + ") disse: " + mensagemPrivada[1]);
+					}
+					
+				}
+				else if(msg.startsWith("/")){
+					escritor.println("Comando nao reconhecido");
+				}else{
 					escritor.println(nomeCliente + " disse: " + msg);
 				}
+				
 			}
 			
 		} catch (IOException e) {
@@ -42,5 +73,14 @@ public class GerenciadorDeCliente extends Thread {
 			
 		}
 	}
-
+	
+	/* GET & SET*/
+	public PrintWriter getEscritor() {
+		return escritor;
+	}
+	
+	public String getNomCliente() {
+		return nomeCliente;
+	}
+	
 }
